9e6de3cbfd09dbbe3eb714505ec8b36f938db94c
llahti <llahti@student.hive.fi>
2021-01-01 14:57:48 +0200

    Update viewService to visitService

---
+++ client/src/services/visitsService.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import auth from '../utils/auth'
+const baseUrl = '/api/visits'
+
+const addVisit = async userObject => {
+	const resp = await axios.post(baseUrl, userObject, auth.config)
+	return resp.data
+}
+
+const visitHistory = async to => {
+	const resp = await axios.get(`${baseUrl}?to=${to}`, auth.config)
+	return resp.data
+}
+
+export default { addVisit, visitHistory }
