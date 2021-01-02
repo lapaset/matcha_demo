0e71e4fbccbf66ebbf269c32bca5c1f1d9bd34a0
llahti <llahti@student.hive.fi>
2021-01-02 18:02:58 +0200

    Change logs

--- utils/middleware.js
+++ utils/middleware.js
@@ -9,7 +9,7 @@ const tokenExtractor = (req, res, next) => {
 const requestLogger = (req, res, next) => {
 	console.log('Method:', req.method)
 	console.log('Path:  ', req.path)
-	console.log('Body:  ', req.body)
+	//console.log('Body:  ', req.body)
 	console.log('---')
 	next()
 }
