95df2fd5d73047cd5e7686f98603a315a43044a7
llahti <llahti@student.hive.fi>
2021-01-03 16:14:53 +0200

    Fix redirect on logout

--- client/src/services/logoutService.js
+++ client/src/services/logoutService.js
@@ -1,14 +1,10 @@
-import { useHistory } from 'react-router-dom'
-
 const handleLogout = (wsClient, user_id) => {
-	const history = useHistory()
-
 	localStorage.clear()
 	wsClient.current.send(JSON.stringify(({
 		type: 'close',
 		from: user_id
 	})))
-	history.push('/login')
+	window.location.href='/login'
 }
 
 export default { handleLogout }
